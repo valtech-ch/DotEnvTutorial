c4598c7ff568c8bbc7fbcee115671bde5724e396
Jason Wilson <jason.wilson@valtech.com>
2020-12-08 03:04:00 +0100

    Add sample invocations

---
+++ index.js
@@ -0,0 +1,15 @@
+// Retrieve a value from the env object and assign it to a constant
+const THE_BEST_FRIES = process.env.THE_BEST_FRIES
+
+console.log('Who has the best fries?')
+console.log(`*whispers* ${THE_BEST_FRIES}...`)
+
+// Use destructuring to assign multiple variables at once
+const { DATABASE_USER, SPECIAL_API_KEY } = process.env
+
+console.log(`
+Two things we should never share publicly...
+- our database name, and
+- our API key
+(${DATABASE_USER} and ${SPECIAL_API_KEY} respectively)
+`)
